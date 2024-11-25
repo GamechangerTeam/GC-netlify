f8b892074f5b1841f7fd2b57a8fdbd4cf80082c6
Crazy <crazy.kitaiets228@mail.ru>
2024-11-26 04:56:03 +0500

    fix about page

--- src/app/about/sections/homeScreen/HomeScreen.jsx
+++ src/app/about/sections/homeScreen/HomeScreen.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 import ButtonText from "@/components/ui/Button-text";
-import "./homeScreen.scss"
+import "./homeScreen.scss";
 
 export default function HomeScreen() {
   return (
-    <div className='home-screen'>
-        <h1 className="home-screen__title">Заголовок для <br /> главного экрана</h1>
-        <div className="home-screen__buttons">
-          <ButtonText children={"Сертификаты"} className={"home-screen__button"}/>
-          <ButtonText children={"Заказать звонок"} className={"home-screen__button"}/>
-        </div>
+    <div className="home-screen">
+      <h1 className="home-screen__title">
+        Заголовок для <br /> главного экрана
+      </h1>
+      <div className="home-screen__buttons">
+        <ButtonText className={"home-screen__button"}>Сертификаты</ButtonText>
+        <ButtonText className={"home-screen__button"}>
+          Заказать звонок
+        </ButtonText>
+      </div>
     </div>
-  )
+  );
 }
